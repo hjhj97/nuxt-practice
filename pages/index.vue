60cfbb2f99f03cd30356e5d3bc28666b974a4a12
JuHeonHa <hjhjaueon97@naver.com>
2023-04-09 02:16:52 +0900

    applu NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink :to="{ name: 'index' }">Home</NuxtLink>
+            <NuxtLink :to="{ name: 'about' }">About</NuxtLink>
+            <NuxtLink :to="{ name: 'products' }">Product</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <h2>Home</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio optio quis adipisci tenetur pariatur voluptatem id
